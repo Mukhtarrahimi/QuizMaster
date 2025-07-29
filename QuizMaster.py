9658e0d57f5798ab4658e77777b7a089c536c72b
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-07-29 14:25:34 +0430

    menu function code.

---
+++ QuizMaster.py
@@ -0,0 +1,11 @@
+def menu():
+    print("QuizMaster - Quiz System")
+    print(".1 Add new question")
+    print(".2 Start quiz")
+    print(".3 View all questions")
+    print(".4 delete question")
+    print(".5 Save Questions to File")
+    print(".6 Load Questions from File")
+    print(".7 Exit")
+    
+menu()
