108982c1b6f2d4ef9f58b9d7d45c57f771e0f1fa
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-07-29 14:41:05 +0430

    add_question function code.

--- QuizMaster.py
+++ QuizMaster.py
@@ -1,3 +1,20 @@
+questions = []
+def add_question():
+    print("Add a new question")
+    question_text = input("Enter your question: ")
+    options = []
+    for op in range(4):
+        op = input(f"option {op +1}:")
+        options.append(op)
+    correct = int(input("correct answer(1-4): ")) 
+    questions.append({
+        "question": question_text,
+        "options": options,
+        "answer": correct
+    })
+    print("Question added successfully.")
+    
+    
 def menu():
     print("QuizMaster - Quiz System")
     print(".1 Add new question")
@@ -9,3 +26,8 @@ def menu():
     print(".7 Exit")
     
 menu()
+
+while True:
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        add_question()
